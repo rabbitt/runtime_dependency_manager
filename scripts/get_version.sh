dc71aeee4666d0c1479055a0ee6df3d00147e620
Carl P. Corliss <rabbitt@gmail.com>
2024-06-04 01:21:07 -0400

    Add license - add GPLv3 LICENSE file - add GPL header to all relevant files

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# runtime_dependency_manager
+# Copyright (C) 2024 Carl Corliss
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Get the latest tag
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
 if [ -z "$LATEST_TAG" ]; then
